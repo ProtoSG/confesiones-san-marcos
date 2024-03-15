f0f1390abf1aecc6bd5b033d4579862139b08cc1
DiegoSG-exe <diego.salazar4@unmsm.edu.pe>
2024-03-14 20:27:25 -0500

    deletteAuth

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from 'astro/config';
-import auth from "auth-astro";
 
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), auth()],
+  integrations: [tailwind()],
   output: 'server',
   adapter: vercel()
 });
